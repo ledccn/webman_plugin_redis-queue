abc41397b3cf7ef8d98a9af73df8c6364f91b946
david <nz1985@qq.com>
2023-11-20 02:33:40 +0800

    新增：启用单进程Job任务的配置项

--- src/config/plugin/ledc/redis-queue/process.php
+++ src/config/plugin/ledc/redis-queue/process.php
@@ -7,9 +7,11 @@ return [
         'constructor' => [
             // 消费者类目录
             'consumer_dir' => app_path() . '/queue/redis',
+            // 处理失败队列的回调函数
             'failed_callback' => function ($package, $connection_name) {
-                var_dump($connection_name, $package);
-            }
+            },
+            // 启用单进程Job任务
+            'single_jobs' => true,
         ]
     ],
 ];
